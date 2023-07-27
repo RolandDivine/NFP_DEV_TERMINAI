5aeba66c81c140d6e92c65914d1dac1595b077f9
Roland Divine <45848152+RolandDivine@users.noreply.github.com>
2023-07-27 14:10:04 +0100

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our application.</p>
+      {/* Add more content and components as needed */}
+    </div>
+  );
+};
+
+export default Home;
